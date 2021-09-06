8590a2ab13d67abe77f68295dd3ab2ad1735d6d8
TCxDouglas <douglasalexander683@gmail.com>
2021-09-05 22:42:25 -0600

    En Emision

--- wakai-otaku/src/Components/Estrenos.jsx
+++ wakai-otaku/src/Components/Estrenos.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import { Slider } from './Slider/Slider';
 import { Miniaturas } from './Animes/items/Miniaturas';
+import { Emision } from './Animes/Emision';
 
 export const Estrenos = () => {
 
     return (
         <>
+            <Emision></Emision>
             
-            <section id="content">
-                <h2 className="subheader">Estrenos mas recientes</h2>
-
-                <div id="articles">
-                    <Miniaturas></Miniaturas>
-                    <Miniaturas></Miniaturas>
-                    <Miniaturas></Miniaturas>
-                    <Miniaturas></Miniaturas>
-                    <Miniaturas></Miniaturas>
-                    <Miniaturas></Miniaturas>
-                </div>
-
-            </section>
         </>
     )
 }
