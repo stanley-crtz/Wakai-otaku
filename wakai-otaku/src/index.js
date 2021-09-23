df1e4b2e32f83ba215d71f481dee51bf7641ec70
TCxDouglas <douglasalexander683@gmail.com>
2021-09-22 23:56:31 -0600

    Configurando Firebase

--- wakai-otaku/src/index.js
+++ wakai-otaku/src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './Assets/CSS/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseAppProvider } from 'reactfire'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSkps3B--SE2leTkAd-l0oYSVXJ_4D7Bk",
+  authDomain: "test-pruebas-12778.firebaseapp.com",
+  databaseURL: "https://test-pruebas-12778-default-rtdb.firebaseio.com",
+  projectId: "test-pruebas-12778",
+  storageBucket: "test-pruebas-12778.appspot.com",
+  messagingSenderId: "36464234697",
+  appId: "1:36464234697:web:232479e37c5a1a76a43d18"
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
